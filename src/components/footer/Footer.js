a3ace3f6b9da00556406ea47249dce2876ea938f
Matias Benites <matiasbenites665@gmail.com>
2022-08-09 00:50:55 -0300

    Added content for footer like contact data & social media

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,34 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer-container'>
-        <div className='footer-logo'></div>
+    <div className="footer">
+      <div className="footer-info">
+        <h1>Your name</h1>
+        <p>Based in Corrientes,AR</p>
+      </div>
+      <div className="footer-contact">
+        <h3>Contact me</h3>
+        <p>And let's get down to mark</p>
+      </div>
+      <div className="footer-sns">
+        <div className="design-by">Design by Benites Matias</div>
+        <div className="sns-links">
+          <a href="" target="_blank">
+            <i className="fab fa-linkedin linkedin"></i>
+          </a>
+          <a href="" target="_blank">
+            <i className="fab fa-github github"></i>
+          </a>
+          <a href="" target="_blank">
+            <i className="fab fa-instagram instagram"></i>
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
